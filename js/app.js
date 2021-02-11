63203b66ccd54362540391e87c6ed06005061936
Arnaud Douglas Ardans II <github@arnaudardans.com>
2021-02-10 16:20:05 -0800

    complete input form, updates nametag

--- js/app.js
+++ js/app.js
@@ -5,5 +5,18 @@ const nameDisplay = document.getElementById('name-display');
 changeButton.addEventListener('click', () => {
   // console.log('Button Clicked');
   nameDisplay.textContent = nameInput.value;
+  nameDisplay.classList.add("extra-bold");
   nameInput.value = '';
 });
+
+nameInput.addEventListener('keyup', function(event) {
+  var key = event.key || event.keyCode;
+  if (key === 'Enter' || key === 13) {
+    event.preventDefault
+    
+    changeButton.click();  // document.getElementById('name-change-button').click();
+  }
+});
+
+console.log('Hello')
+
